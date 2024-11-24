c982d0a38d04af77f8f958c2f24824d852f4cf66
Nishit Vas <mail@nishitvas.me>
2024-11-24 15:41:09 -0800

    add sketchy config

---
+++ sketchybar/.config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+  text = "RecMonoLinear Nerd Font", -- Used for text
+  numbers = "RecMonoLinear Nerd Font", -- Used for numbers
+
+  -- Unified font style map
+  style_map = {
+    ["Regular"] = "Regular",
+    ["Semibold"] = "Bold", --"Semibold",
+    ["Bold"] = "Bold",
+    ["Heavy"] = "Bold", --"Heavy",
+    ["Black"] = "Bold", --"Black",
+  }
+}
